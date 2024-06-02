836b60efff8f5244676a4f2195a9300c1cf5d523
cahdmus <bayingpan@gmail.com>
2024-06-02 21:38:07 +0200

    human input, computer output and comparison working. also looks cute

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -12,61 +12,39 @@ function getComputerChoice() {
             return "Hmmmm... I think I messed up somewhere";
     }
 }
-
-console.log(`Computer played ${getComputerChoice()} !`);
-
+    
 // Human input
-function getHumanChoice() {
+function getHumanChoice () {
     return prompt("THIS IS THE ROCK PAPER SCISSORS TOURNAMENT !\nWhat are you going to play ?", "Rock, paper or scissors ?");
 }
-
+    
 // Logic
 let humanScore = 0;
 let computerScore = 0;
 
 const winMessage = "Oh my god, you win !";
-const LoseMessage = "Boohoo you loose...";
-const DrawMessage = "It's a draw";
+const loseMessage = "Boohoo you loose...";
+const drawMessage = "It's a draw";
 
 function playRound(humanChoice, computerChoice) {
-    switch (humanChoice.toLowerCase().trim()) {
-        case "rock":
-            switch (computerChoice) {
-                case "rock":
-                    return DrawMessage;
-                case "paper":
-                    computerScore = + 1;
-                    return LoseMessage;
-                case "scissors":
-                    humanScore = + 1;
-                    return winMessage;
+    let humanShort = humanChoice.toLowerCase().trim();
+    if (humanShort == computerChoice) {
+        return drawMessage;
+    }
+    else if (humanShort == "rock" && computerChoice == "scissors" 
+            || humanShort == "paper" && computerChoice == "rock"
+            || humanShort == "scissors" && computerChoice == "paper") {
+        humanScore++;
+        return winMessage;
             }
-        case "paper":
-            switch (computerChoice) {
-                case "rock":
-                    humanScore = + 1;
-                    return winMessage;
-                case "paper":
-                    return DrawMessage;
-                case "scissors":
-                    computerScore = + 1;
-                    return LoseMessage;
-            }
-        case "scissors":
-            switch (computerChoice) {
-                case "rock":
-                    computerScore = + 1;
-                    return LoseMessage;
-                case "paper":
-                    humanScore = + 1;
-                    return winMessage;
-                case "scissors":
-                    return DrawMessage;
-            }
-        default:
-            return "What just happened ?"
+    else {
+        computerScore++;
+        return loseMessage;
     }
 }
-
-console.log(playRound(getHumanChoice(), getComputerChoice()));
-// console.log(`Human played ${getHumanChoice()} !`);
+    
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+    
+alert(`Computer played ${computerSelection}, and human played ${humanSelection}. What a thrilling game !!\nTherefore the results are in : ${playRound(humanSelection, computerSelection)}`);    
+    
