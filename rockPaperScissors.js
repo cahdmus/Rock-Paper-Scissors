da39ea2c3a1c52d8dc2f202edf39e0f19b355e1c
cahdmus <bayingpan@gmail.com>
2024-06-02 19:24:32 +0200

    human input and logic of round added. It looks like it should be working but it's not

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,18 +1,72 @@
 // Computer Output
 function getComputerChoice() {
-    let result = Math.floor(Math.random() * 3);
-    switch(result) {
+    let computerResult = Math.floor(Math.random() * 3);
+    switch (computerResult) {
         case 0:
-            return "Rock";
+            return "rock";
         case 1:
-            return "Paper";
+            return "paper";
         case 2:
-            return "Scissors";
+            return "scissors";
         default:
             return "Hmmmm... I think I messed up somewhere";
     }
 }
 
-console.log(`Computer plays ${getComputerChoice()} !`);
+console.log(`Computer played ${getComputerChoice()} !`);
 
-// Player
+// Human input
+function getHumanChoice() {
+    return prompt("THIS IS THE ROCK PAPER SCISSORS TOURNAMENT !\nWhat are you going to play ?", "Rock, paper or scissors ?");
+}
+
+// Logic
+let humanScore = 0;
+let computerScore = 0;
+
+const winMessage = "Oh my god, you win !";
+const LoseMessage = "Boohoo you loose...";
+const DrawMessage = "It's a draw";
+
+function playRound(humanChoice, computerChoice) {
+    switch (humanChoice.toLowerCase().trim()) {
+        case "rock":
+            switch (computerChoice) {
+                case "rock":
+                    return DrawMessage;
+                case "paper":
+                    computerScore = + 1;
+                    return LoseMessage;
+                case "scissors":
+                    humanScore = + 1;
+                    return winMessage;
+            }
+        case "paper":
+            switch (computerChoice) {
+                case "rock":
+                    humanScore = + 1;
+                    return winMessage;
+                case "paper":
+                    return DrawMessage;
+                case "scissors":
+                    computerScore = + 1;
+                    return LoseMessage;
+            }
+        case "scissors":
+            switch (computerChoice) {
+                case "rock":
+                    computerScore = + 1;
+                    return LoseMessage;
+                case "paper":
+                    humanScore = + 1;
+                    return winMessage;
+                case "scissors":
+                    return DrawMessage;
+            }
+        default:
+            return "What just happened ?"
+    }
+}
+
+console.log(playRound(getHumanChoice(), getComputerChoice()));
+// console.log(`Human played ${getHumanChoice()} !`);
