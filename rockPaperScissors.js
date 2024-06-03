46573d47c3d7e9aba4da51bb2c445b6bc9431900
cahdmus <bayingpan@gmail.com>
2024-06-03 17:40:42 +0200

    declareWinner and getPlayersChoices added, seems to be working

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,50 +1,89 @@
-// Computer Output
-function getComputerChoice() {
-    let computerResult = Math.floor(Math.random() * 3);
-    switch (computerResult) {
-        case 0:
-            return "rock";
-        case 1:
-            return "paper";
-        case 2:
-            return "scissors";
-        default:
-            return "Hmmmm... I think I messed up somewhere";
+ function playGame() {
+    // Computer Output
+    function getComputerChoice() {
+        let computerResult = Math.floor(Math.random() * 3);
+        switch (computerResult) {
+            case 0:
+                return "rock";
+            case 1:
+                return "paper";
+            case 2:
+                return "scissors";
+            default:
+                return "Hmmmm... I think I messed up somewhere";
+        }
     }
-}
-    
-// Human input
-function getHumanChoice () {
-    return prompt("THIS IS THE ROCK PAPER SCISSORS TOURNAMENT !\nWhat are you going to play ?", "Rock, paper or scissors ?");
-}
-    
-// Logic
-let humanScore = 0;
-let computerScore = 0;
+        
+    // Human input
+    function getHumanChoice () {
+        return prompt("THIS IS THE ROCK PAPER SCISSORS TOURNAMENT !\nWhat are you going to play ?", "Rock, paper or scissors ?");
+    }
 
-const winMessage = "Oh my god, you win !";
-const loseMessage = "Boohoo you loose...";
-const drawMessage = "It's a draw";
+    const winMessage = "Oh my god, you win this round !";
+    const loseMessage = "Boohoo you loose this round...";
+    const drawMessage = "It's a draw";
+    const errorMessage = "You played an unauthorized hand ! Disgusting..."
 
-function playRound(humanChoice, computerChoice) {
-    let humanShort = humanChoice.toLowerCase().trim();
-    if (humanShort == computerChoice) {
-        return drawMessage;
+    let humanScore = 0;
+    let computerScore = 0;
+
+    function playRound(humanChoice, computerChoice) {
+        let humanShort = humanChoice.toLowerCase().trim();
+        if (humanShort != "rock" 
+                && humanShort != "paper" 
+                && humanShort != "scissors") {
+            return errorMessage;
+        }
+        else if (humanShort == computerChoice) {
+            return drawMessage;
+        }
+        else if (humanShort == "rock" && computerChoice == "scissors" 
+                || humanShort == "paper" && computerChoice == "rock"
+                || humanShort == "scissors" && computerChoice == "paper") {
+            humanScore++;
+            return winMessage;
+                }
+        else {
+            computerScore++;
+            return loseMessage;
+        }
     }
-    else if (humanShort == "rock" && computerChoice == "scissors" 
-            || humanShort == "paper" && computerChoice == "rock"
-            || humanShort == "scissors" && computerChoice == "paper") {
-        humanScore++;
-        return winMessage;
-            }
-    else {
-        computerScore++;
-        return loseMessage;
+
+    function getPlayersChoices() {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        
+        return `Computer played ${computerSelection}, and human played ${humanSelection}. What a thrilling game !!\nTherefore the results are in : 
+        ${playRound(humanSelection, computerSelection)}`; 
     }
+
+    alert(getPlayersChoices());
+    alert(getPlayersChoices());
+    alert(getPlayersChoices());
+    alert(getPlayersChoices());
+    alert(getPlayersChoices());
+
+    const gameDrawMessage = `Well, whatever you did the computer has a score of ${computerScore}, and your score is ${humanScore} so it's a draw !`;
+    const gameLoseMessage = `With a score of ${computerScore}, computer wins !`;
+    const gameWinMessage = `With a score of ${humanScore}, you win !`;
+    const gameErrorMessage = `Hmmm... the computer has a score of ${computerScore}, and your score is ${humanScore} but to me honest I don't know what happened...`;
+    
+    function declareWinner(player1, player2) {
+        if (player1 == player2) {
+            return gameDrawMessage;
+        }
+        else if (player1 < player2) {
+            return gameLoseMessage;
+        }
+        else if (player1 > player2) {
+            return gameWinMessage;
+        }
+        else {
+            return gameErrorMessage;
+        }
+    } 
+    
+    return declareWinner(humanScore, computerScore);
 }
-    
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-    
-alert(`Computer played ${computerSelection}, and human played ${humanSelection}. What a thrilling game !!\nTherefore the results are in : ${playRound(humanSelection, computerSelection)}`);    
-    
+
+alert(playGame());
